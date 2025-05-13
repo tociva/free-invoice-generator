62d84dad1c7da7b337a42217161a09a87d5a5109
Prince Francis <princekfrancis@gmail.com>
2025-05-13 13:54:59 +0530

    invoice creation form - customer details.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule], // ✅ Add this
-  template: `<router-outlet></router-outlet>`,
+  imports: [RouterModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule, 
+  ],
+  styleUrls: ['./app.component.scss'],
+  templateUrl: './app.component.html',
 })
 export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { InvoiceComponent } from './components/invoice/invoice.component';
 import { TemplatesComponent } from './components/templates/templates.component';
 
 export const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'invoice', component: InvoiceComponent },
+  { path: '', component: InvoiceComponent },
   { path: 'templates', component: TemplatesComponent }
 ];
