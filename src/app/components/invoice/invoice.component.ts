fe58011f375981959fe66d9ad82e5045dd1cacc5
Prince Francis <princekfrancis@gmail.com>
2025-05-13 13:54:59 +0530

    invoice creation form - customer details.

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -1,42 +1,50 @@
 import { Component } from '@angular/core';
-import { Location } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { AgGridModule } from 'ag-grid-angular';
+import { ColDef, GridOptions } from 'ag-grid-community';
+import { FormColumnDef } from '../../../util/form-column-def.type';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
   templateUrl: './invoice.component.html',
   styleUrls: ['./invoice.component.scss'],
-  imports: [
-    FormsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule
-  ]
+  imports: [AgGridModule, MatCardModule],
+  
 })
 export class InvoiceComponent {
-  clientName = '';
-  invoiceDate = '';
-  amount = 0;
 
-  constructor(private location: Location) {}
 
-  generateInvoice() {
-    console.log('Invoice generated:', {
-      clientName: this.clientName,
-      invoiceDate: this.invoiceDate,
-      amount: this.amount
-    });
-  }
+  gridOptions: GridOptions = {
+    domLayout: 'autoHeight',
+    rowHeight: 30,
+  };
+  
+  defaultColDef = {
+    sortable: false,
+    resizable: false,
+    suppressMenu: true,
+    editable: false
+  };
 
-  goBack() {
-    this.location.back();
-  }
+  customerColumnDefs: ColDef<FormColumnDef>[] = [
+    { field: 'label', headerName: '', width: 150 },
+    { field: 'value', headerName: '', width: 200, editable: true }
+  ];
+  
+
+  customerRowData: FormColumnDef[] = [
+    { label: 'Name', value: 'Tociva Technologies' },
+    { label: 'Line 1', value: '123 Main St' },
+    { label: 'Line 2', value: 'St.Louis, MO' },
+    { label: 'Street', value: 'Carolina St' },
+    { label: 'City', value: 'St.Louis,' },
+    { label: 'Postal Code', value: '63101' },
+    { label: 'State', value: 'Missouri' },
+    { label: 'Country', value: 'United States' },
+    { label: 'E-Mail', value: 'info@tociva.com' },
+    { label: 'Mobile', value: '1234567890' }
+  ];
+
+  
 }
