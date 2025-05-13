c4e295253992516ef0244c0db1d60203a774185f
Prince Francis <prince.francis@spectrio.com>
2025-05-13 12:05:34 +0530

    landing-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InvoiceComponent } from './components/invoice/invoice.component';
+import { TemplatesComponent } from './components/templates/templates.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'invoice', component: InvoiceComponent },
+  { path: 'templates', component: TemplatesComponent }
+];
