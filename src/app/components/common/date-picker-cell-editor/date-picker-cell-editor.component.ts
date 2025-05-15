b9b739f48e4804625b85f28da3bc8b6538fe574b
Adarsh Edathodathil <adarshedathodathil@gmail.com>
2025-05-15 13:01:53 +0530

    date picker allignment done

--- src/app/components/common/date-picker-cell-editor/date-picker-cell-editor.component.ts
+++ src/app/components/common/date-picker-cell-editor/date-picker-cell-editor.component.ts
@@ -2,17 +2,15 @@ import { Component } from '@angular/core';
 import { ICellEditorAngularComp } from 'ag-grid-angular';
 import { MatDatepicker, MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-date-picker-cell-editor',
-  imports: [MatDatepicker,
-     MatFormFieldModule,
-    MatInputModule,
+  imports: [
+    MatDatepicker,
+    MatFormFieldModule,
     MatDatepickerModule,
-    MatNativeDateModule,
-    FormsModule
+    FormsModule,
+
   ],
   templateUrl: './date-picker-cell-editor.component.html',
   styleUrl: './date-picker-cell-editor.component.scss'
@@ -24,13 +22,13 @@ export class DatePickerCellEditor implements ICellEditorAngularComp {
   }
 
   getValue(): string {
-  const d = this.dateValue;
-  if (!d) return '';
-  const yyyy = d.getFullYear();
-  const mm = String(d.getMonth() + 1).padStart(2, '0');
-  const dd = String(d.getDate()).padStart(2, '0');
-  return `${yyyy}-${mm}-${dd}`;
-}
+    const d = this.dateValue;
+    if (!d) return '';
+    const yyyy = d.getFullYear();
+    const mm = String(d.getMonth() + 1).padStart(2, '0');
+    const dd = String(d.getDate()).padStart(2, '0');
+    return `${yyyy}-${mm}-${dd}`;
+  }
 
 
 }
