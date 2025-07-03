e80e1da837005c72664cab1b6bb724b0bd3ec609
Prince Francis <princekfrancis@gmail.com>
2025-07-03 16:55:44 +0530

    added .html instead of .json for tracking url.

--- src/app/services/cloud-data.service.ts
+++ src/app/services/cloud-data.service.ts
@@ -7,14 +7,19 @@ export class CloudDataService {
 
   // eslint-disable-next-line class-methods-use-this
   async trackRouteChange(path: string) {
-    await fetch(`/cloud-data/route-${encodeURIComponent(path)}.json`)['catch']((error) => {
+    try {
+      const [basePath, query = ''] = path.split('?');
+      const finalPath = `${basePath}.html${query ? `?${query}` : ''}`;
+      await fetch(`/cloud-data/route${finalPath}`);
+    } catch (error) {
       console.error('Error tracking route change:', error);
-    });
+    }
   }
+  
 
   // eslint-disable-next-line class-methods-use-this
   async trackEvent(eventName: string) {
-    await fetch(`/cloud-data/event-${encodeURIComponent(eventName)}.json`)['catch']((error) => {
+    await fetch(`/cloud-data/event/${encodeURIComponent(eventName)}.html`)['catch']((error) => {
       console.error('Error tracking event:', error);
     });
   }
