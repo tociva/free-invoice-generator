1a7dc36834bd302024b14e5074fd210722f4d90c
Abin Mathew <abin02066@gmail.com>
2025-12-22 10:53:23 +0530

    Template-layout-free-invoice-generator

---
+++ src/app/components/invoice/simple-invoice/simple-invoice.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-invoice',
+  imports: [],
+  templateUrl: './simple-invoice.html',
+  styleUrl: './simple-invoice.css',
+})
+export class SimpleInvoice {
+
+}
