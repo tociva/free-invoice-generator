092a48574a7427b95a658b99032df12013462b3e
Abin Mathew <abin02066@gmail.com>
2025-12-23 18:05:48 +0530

    Json file upload custom directive

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { provideAppIcon } from '../../provider/icon-provider';
 import { NgIcon } from '@ng-icons/core';
 import { Router } from '@angular/router';
+import { FileUpload } from '../shared/file-upload/file-upload';
 
 @Component({
   selector: 'app-home',
-  imports: [NgIcon],
+  imports: [NgIcon,FileUpload],
   templateUrl: './home.html',
   styleUrl: './home.css',
   providers :[provideAppIcon()],
