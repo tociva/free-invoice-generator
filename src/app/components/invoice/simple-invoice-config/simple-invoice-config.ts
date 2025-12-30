221ca826cd282b7c908c06d46533cf994abf3db6
Abin Mathew <abin02066@gmail.com>
2025-12-30 16:11:35 +0530

    Update components, styles, and signal forms

---
+++ src/app/components/invoice/simple-invoice-config/simple-invoice-config.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-invoice-config',
+  imports: [],
+  templateUrl: './simple-invoice-config.html',
+  styleUrl: './simple-invoice-config.css',
+})
+export class SimpleInvoiceConfig {
+
+}
