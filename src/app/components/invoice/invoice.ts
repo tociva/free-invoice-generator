014d8e02caef6bdafec98a838bd6ec2c90983605
Adarsh_E <adarshedathodathil@gmail.com>
2026-01-08 16:25:42 +0530

    added advanced invoice page.

--- src/app/components/invoice/invoice.ts
+++ src/app/components/invoice/invoice.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { InvoiceOrganizationComponent } from "./invoice-organization/invoice-organization";
+import { FileUpload } from "../shared/file-upload/file-upload";
+import { InvoiceCustomerComponent } from "./invoice-customer/invoice-customer";
+import { InvoiceDetailsComponent } from "./invoice-details/invoice-details";
+import { InvoiceItemsComponent } from "./invoice-items/invoice-items";
+import { SelectTemplateComponent } from "./select-template/select-template";
+import { PreviewInvoiceComponent } from "./preview-invoice/preview-invoice";
 
 @Component({
   selector: 'app-invoice',
-  imports: [],
+  imports: [InvoiceOrganizationComponent, FileUpload, InvoiceCustomerComponent, InvoiceDetailsComponent, InvoiceItemsComponent, SelectTemplateComponent, PreviewInvoiceComponent],
   templateUrl: './invoice.html',
   styleUrl: './invoice.css',
 })
 export class Invoice {
+  currentStep = signal(1);
 
+  steps = [
+    { id: 1, label: 'My Organization Info & Logo' },
+    { id: 2, label: 'Customer Details' },
+    { id: 3, label: 'Invoice Details' },
+    { id: 4, label: 'Items and Summary' },
+    { id: 5, label: 'Select a template' },
+    { id: 6, label: 'Preview and Download' },
+  ];
+
+  isFirstStep = computed(() => this.currentStep() === 1);
+  isLastStep = computed(() => this.currentStep() === this.steps.length);
+
+  goToStep(stepId: number): void {
+    this.currentStep.set(stepId);
+  }
 }
