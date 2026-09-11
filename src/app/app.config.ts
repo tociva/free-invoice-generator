c84be4720a03035ad252af72a8c805672c49db96
Prince Francis <princekfrancis@gmail.com>
2026-09-11 18:08:18 +0530

    build error fix.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,19 +4,6 @@ import { provideRouter } from '@angular/router';
 import { provideTngIcons } from '@tailng-ui/icons';
 import { provideAppTheme } from '../theme';
 import { routes } from './app.routes';
-import { CountryEffects } from './components/invoice/store/effects/country.effects';
-import { CurrencyEffects } from './components/invoice/store/effects/currency.effects';
-import { countryReducer } from './components/invoice/store/reducer/country.reducer';
-import { currencyReducer } from './components/invoice/store/reducer/currency.reducer';
-import { TaxEffects } from './components/invoice/store/effects/tax.effects';
-import { taxReducer } from './components/invoice/store/reducer/tax.reducer';
-import { dateFormatReducer } from './components/invoice/store/reducer/date-format.reducer';
-import { DateFormatEffects } from './components/invoice/store/effects/date-format.effects';
-import { invoiceReducer } from './components/invoice/store/reducer/invoice.reducer';
-import { provideHttpClient } from '@angular/common/http';
-import { templateReducer } from './components/templates/store/reducer/template.reducer';
-import { TemplateEffects } from './components/templates/store/effects/tempate.effects';
-import { tagReducer } from './components/templates/store/reducer/tag.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { App } from './app/app';
 
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
-
-ModuleRegistry.registerModules([AllCommunityModule]);
-
-bootstrapApplication(AppComponent, appConfig)['catch']((err) => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
